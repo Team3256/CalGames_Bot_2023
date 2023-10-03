39bef8d9c9107877de1ff642901c957d495a50e9
3LucasZ <lucas.zheng@warriorlife.net>
2023-10-02 20:35:59 -0700

    Delineation of code to make it neater in RobotContainer.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,18 +63,19 @@ public class RobotContainer implements CANTestable, Loggable {
   private final CommandXboxController driver = new CommandXboxController(0);
   private final CommandXboxController operator = new CommandXboxController(1);
   private final CommandXboxController tester = new CommandXboxController(2);
+
   private SwerveDrive swerveSubsystem;
   private Intake intakeSubsystem;
   private Elevator elevatorSubsystem;
   private Arm armSubsystem;
   private LED ledSubsystem;
+
   private GamePiece currentPiece = GamePiece.CONE;
   private GridScoreHeight currentScoringPreset = GridScoreHeight.LOW;
-
   private SubstationLocation doubleSubstationLocation = SubstationLocation.RIGHT_SIDE;
+
   private RobotSimulation robotSimulation;
   private SendableChooser<Mode> modeChooser;
-
   private AutoPaths autoPaths;
 
   private final ArrayList<CANTestable> canBusTestables = new ArrayList<>();
@@ -91,7 +92,6 @@ public class RobotContainer implements CANTestable, Loggable {
       System.out.println("LED Subsystem Enabled");
       configureLEDStrip();
     } else System.out.println("LED Subsystem NOT Enabled");
-
     if (kIntakeEnabled) {
       System.out.println("Intake Subsystem Enabled");
       configureIntake();
