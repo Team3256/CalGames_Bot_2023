3a080757c6aa22c3b3609e2eb71fabb6533a3729
Josh <102478723+realjoshuau@users.noreply.github.com>
2023-10-07 10:49:40 -0700

    funny auto
    Co-Authored-By: Smogless <92347357+Smogless@users.noreply.github.com>

--- src/main/java/frc/robot/auto/AutoPaths.java
+++ src/main/java/frc/robot/auto/AutoPaths.java
@@ -257,6 +257,11 @@ public class AutoPaths {
                 // auto
                 AutoChooser.createSinglePath("Node8-Mobility", node8Mobility);
 
+                Command node2Mobility = autoBuilder.createPath("Node2-Mobility", kEngagePathConstraints, true); // funny
+                // cube
+                // auto
+                AutoChooser.createSinglePath("Node2-Mobility", node2Mobility);
+
                 AutoChooser.sendChooserToDashboard("Auto Chooser");
         }
 
