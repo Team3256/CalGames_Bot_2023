8c54b47eadb753e42c7c248623f63a1fccae8f7d
3LucasZ <lucas.zheng@warriorlife.net>
2023-10-08 08:17:08 -0700

    Create ZeroArmSensor.java

---
+++ src/main/java/frc/robot/arm/commands/ZeroArmSensor.java
@@ -0,0 +1,36 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.arm.commands;
+
+import frc.robot.arm.Arm;
+import frc.robot.helpers.DebugCommandBase;
+
+public class ZeroArmSensor extends DebugCommandBase {
+  private final Arm armSubsystem;
+
+  public ZeroArmSensor(Arm armSubsystem) {
+    addRequirements(armSubsystem);
+    this.armSubsystem = armSubsystem;
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    armSubsystem.zeroArmEncoderGroundRelative();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    super.end(interrupted);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
